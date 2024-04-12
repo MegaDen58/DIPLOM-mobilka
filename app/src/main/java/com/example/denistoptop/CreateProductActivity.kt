666e5bdecb413a713bda727fca05ddfacee656e8
MegaDen58 <extreme.den2004@gmail.com>
2024-04-12 19:23:41 +0300

    загрузка картинки

--- app/src/main/java/com/example/denistoptop/CreateProductActivity.kt
+++ app/src/main/java/com/example/denistoptop/CreateProductActivity.kt
@@ -1,25 +1,31 @@
 package com.example.denistoptop
 
 import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.denistoptop.adapter.ImagesAdapter
-import com.example.denistoptop.dto.ProductDto
 import com.example.denistoptop.service.ProductService
 import okhttp3.MediaType
+import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.util.UUID
+
 
 class CreateProductActivity : AppCompatActivity() {
     private lateinit var galleryButton: Button
@@ -28,10 +34,16 @@ class CreateProductActivity : AppCompatActivity() {
     private lateinit var imagesRecyclerView: RecyclerView
     private lateinit var adapter: ImagesAdapter
     private lateinit var productService: ProductService
+    private val READ_EXTERNAL_STORAGE_REQUEST = 123
+    companion object {
+        const val PICK_IMAGE_REQUEST = 1
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_product)
+        requestStoragePermission()
         imagesRecyclerView = findViewById(R.id.imagesRecyclerView)
         galleryButton = findViewById(R.id.galleryButton)
         submitButton = findViewById(R.id.submitButton)
@@ -46,7 +58,7 @@ class CreateProductActivity : AppCompatActivity() {
         }
 
         submitButton.setOnClickListener {
-            submitProduct()
+            uploadImagesToServer()
         }
 
         productService = Retrofit.Builder()
@@ -82,48 +94,57 @@ class CreateProductActivity : AppCompatActivity() {
             adapter.notifyDataSetChanged()
         }
     }
-
-    private fun submitProduct() {
-        // Получение данных о продукте
-        val nameEditText: EditText = findViewById(R.id.nameEditText)
-        val descriptionEditText: EditText = findViewById(R.id.descriptionEditText)
-        val countEditText: EditText = findViewById(R.id.countEditText)
-        val winterCheckBox: CheckBox = findViewById(R.id.winterCheckBox)
-        val summerCheckBox: CheckBox = findViewById(R.id.summerCheckBox)
-
-        val name = nameEditText.text.toString()
-        val description = descriptionEditText.text.toString()
-        val count = countEditText.text.toString().toInt()
-        val winter = winterCheckBox.isChecked
-        val summer = summerCheckBox.isChecked
-
-        val productJson = "{\"name\": \"$name\", \"description\": \"$description\", \"count\": $count, \"winter\": $winter, \"summer\": $summer, \"images\": ${images.joinToString(prefix = "[", postfix = "]", separator = ",", transform = { "\"$it\"" })}}"
-        val requestBody = RequestBody.create(MediaType.parse("application/json"), productJson)
-        val call = productService.createProduct(requestBody)
-
-        call.enqueue(object : Callback<ProductDto> {
-            override fun onResponse(call: Call<ProductDto>, response: Response<ProductDto>) {
-                if (response.isSuccessful) {
-                    // Обработка успешного ответа
-                    // например, показать сообщение об успешном создании продукта
-                    Toast.makeText(this@CreateProductActivity, "Продукт оптравлен!", Toast.LENGTH_SHORT).show()
-
+    private fun requestStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), READ_EXTERNAL_STORAGE_REQUEST)
+        }
+    }
+    // Проверка результата запроса разрешения
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            READ_EXTERNAL_STORAGE_REQUEST -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Разрешение получено, теперь вы можете выполнять операции чтения внешнего хранилища
                 } else {
-                    // Обработка неудачного запроса
-                    // например, показать сообщение об ошибке
-                    Toast.makeText(this@CreateProductActivity, "пздц1!", Toast.LENGTH_SHORT).show()
+                    // Разрешение не получено, обработайте это соответствующим образом, например, показывая диалоговое окно с объяснением
                 }
             }
-
-            override fun onFailure(call: Call<ProductDto>, t: Throwable) {
-                // Обработка ошибок сети или других ошибок
-                // например, показать сообщение об ошибке
-                Toast.makeText(this@CreateProductActivity, "пздц2!", Toast.LENGTH_SHORT).show()
-            }
-        })
+        }
     }
+    private fun uploadImagesToServer() {
+        for (imageUri in images) {
+            val inputStream = contentResolver.openInputStream(imageUri)
+            val file = File(cacheDir, "temp_image.png")
+            inputStream?.use { input ->
+                file.outputStream().use { output ->
+                    input.copyTo(output)
+                }
+            }
+            val requestFile = RequestBody.create(MediaType.parse("image/png"), file)
+            val body = MultipartBody.Part.createFormData("image", generateRandomFileName(), requestFile)
 
-    companion object {
-        const val PICK_IMAGE_REQUEST = 1
+            val call = productService.uploadImage(body)
+
+            call.enqueue(object : Callback<ResponseBody> {
+                override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                    if (response.isSuccessful) {
+                        val imageUrl = response.body()
+                        // Делайте что-то с URL изображения, например, сохраняйте его в базу данных или отображайте на экране
+                        Toast.makeText(this@CreateProductActivity, "Изображение загружено: $imageUrl", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this@CreateProductActivity, "Ошибка при загрузке изображения", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    Toast.makeText(this@CreateProductActivity, "Ошибка при загрузке изображения: ${t.message}", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+    }
+    private fun generateRandomFileName(): String {
+        val uuid = UUID.randomUUID()
+        return "${uuid.toString()}.png"
     }
 }
--- app/src/main/java/com/example/denistoptop/service/ProductService.kt
+++ app/src/main/java/com/example/denistoptop/service/ProductService.kt
@@ -1,14 +1,20 @@
 package com.example.denistoptop.service
 
 import com.example.denistoptop.dto.ProductDto
+import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface ProductService {
     @POST("products/create")
     //suspend fun createProduct(@Body product: ProductDto): Response<ProductDto>
     fun createProduct(@Body productDto: RequestBody): Call<ProductDto>
+    @Multipart
+    @POST("products/upload/image") // Замените на конечную точку вашего сервера для загрузки изображений
+    fun uploadImage(@Part image: MultipartBody.Part): Call<ResponseBody> // Замените на ваш класс ответа от сервера
 }
