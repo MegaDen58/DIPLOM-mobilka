d4a20bd23daea616c6038c986376a7591e029c1a
MegaDen58 <extreme.den2004@gmail.com>
2024-05-12 21:49:38 +0300

    фильтрация (поиск по описанию и названию)

--- app/src/main/java/com/example/denistoptop/MainActivity.kt
+++ app/src/main/java/com/example/denistoptop/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.denistoptop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -26,13 +30,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cartButton: ImageButton
     private lateinit var mainButton: ImageButton
     private lateinit var toolbar: Toolbar
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchEditText: EditText
+    private var products: List<ProductDto>? = null
+    private var filteredProducts: List<ProductDto>? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-
+        recyclerView = findViewById(R.id.recyclerView)
+        searchEditText = findViewById(R.id.search)
         toolbar = findViewById<Toolbar>(R.id.toolbar_layout)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
@@ -56,18 +65,23 @@ class MainActivity : AppCompatActivity() {
 
         productService = retrofit.create(ProductService::class.java)
 
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                filterProducts(s.toString())
+            }
+        })
+
         // Получение списка продуктов
         productService.getAllProducts().enqueue(object : Callback<List<ProductDto>> {
             override fun onResponse(call: Call<List<ProductDto>>, response: Response<List<ProductDto>>) {
                 if (response.isSuccessful) {
-                    val products = response.body()
-                    val myDataset = products?.map {
-                        val imageUrl = if (it.images.isNotEmpty()) "http://94.228.112.46:8080/api/products/image/${it.images[0]}" else "" // URL первого изображения
-                        MainData(imageUrl, it.name)
-                    } ?: emptyList()
-
-                    recyclerView.adapter = MainAdapter(myDataset)
-                    recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                    products = response.body()
+                    filteredProducts = products
+                    showFilteredProducts()
                 } else {
                     // Обработка неудачного запроса
                 }
@@ -76,6 +90,25 @@ class MainActivity : AppCompatActivity() {
                 // Обработка ошибок сети или других ошибок
             }
         })
+    }
 
+    private fun filterProducts(query: String) {
+        filteredProducts = if (query.isBlank()) {
+            products
+        } else {
+            products?.filter {
+                it.name.contains(query, ignoreCase = true) || it.description.contains(query, ignoreCase = true)
+            }
+        }
+        showFilteredProducts()
+    }
+
+    private fun showFilteredProducts() {
+        val myDataset = filteredProducts?.map {
+            val imageUrl = if (it.images.isNotEmpty()) "http://94.228.112.46:8080/api/products/image/${it.images[0]}" else "" // URL первого изображения
+            MainData(imageUrl, it.name)
+        } ?: emptyList()
+        recyclerView.adapter = MainAdapter(myDataset)
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
